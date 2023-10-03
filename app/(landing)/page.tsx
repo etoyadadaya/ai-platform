ced0ca004e22e1d4bbc7a40e2c0d50aa77e00eed
etoyadadaya <vanyasoup2014@cyber.sibsutis.ru>
2023-10-03 13:09:40 +0700

    clerk auth

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
 
 const LandingPage = () => {
   return (
     <div>
-      <p>Landing Page</p>
+      Landing Page (unprotected)
+      <div>
+        <Link href="/sign-in">
+          <Button>
+            Login
+          </Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>
+            Register
+          </Button>
+        </Link>
+      </div>
     </div>
-  );
+  )
 };
 
 export default LandingPage;
